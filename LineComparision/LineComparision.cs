cf344fa60f1046e4684f5c950da2d33687f4924a
Aniruddha <aniruddha1712@gmail.com>
2022-03-29 17:51:21 +0530

    [Aniruddha]Added: Program to Check equality of two lines

--- LineComparision/LineComparision.cs
+++ LineComparision/LineComparision.cs
@@ -14,17 +14,37 @@ namespace LineComparision
 
             double x1, x2, y1, y2, lengthOfLine1, lengthOfLine2;
 
-            Console.WriteLine("Enter x1 co-ordinate of line :");
+            Console.WriteLine("Enter x1 co-ordinate of line1 :");
             x1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y1 co-ordinate of line :");
+            Console.WriteLine("Enter y1 co-ordinate of line1 :");
             y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter x2 co-ordinate of line :");
+            Console.WriteLine("Enter x2 co-ordinate of line1 :");
             x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y2 co-ordinate of line :");
+            Console.WriteLine("Enter y2 co-ordinate of line1 :");
             y2 = Convert.ToDouble(Console.ReadLine());
 
-            lengthOfLine1= Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            lengthOfLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
             Console.WriteLine("length of first line is:{0}", lengthOfLine1);
+
+            Console.WriteLine("Enter x1 co-ordinate of line2 :");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1 co-ordinate of line2 :");
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2 co-ordinate of line2 :");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2 co-ordinate of line2 :");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            lengthOfLine2 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length of second line is:{0}", lengthOfLine2);
+
+            if (lengthOfLine1 == lengthOfLine2)
+            {
+                Console.WriteLine("Length of Line1 and Line2 is equal");
+            }
+            else
+                Console.WriteLine("Length of Line1 and Line2 is different");
+
 
             Console.ReadLine();
         }
