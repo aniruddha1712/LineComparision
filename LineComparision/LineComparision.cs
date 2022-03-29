4ecaeac65eac5018e81a1c2c963cc1ec5e0e4906
Aniruddha <aniruddha1712@gmail.com>
2022-03-29 16:44:40 +0530

    [Aniruddha]Added: Welcome message

---
+++ LineComparision/LineComparision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    class LineComparision
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Program");
+            Console.ReadLine();
+        }
+    }
+}
